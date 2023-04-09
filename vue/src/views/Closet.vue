ca3523523e282bd8011c8759aee2c11d8ad215b4
Janae Rutland <janae.rutland@gmail.com>
2023-04-09 13:52:31 -0400

    setup router for mycloset

---
+++ vue/src/views/Closet.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ClothesList />
+  </div>
+</template>
+
+<script>
+import ClothesList from "@/components/ClothesList.vue"
+
+export default {
+    components: {
+        ClothesList
+    }
+}
+</script>
+
+<style>
+
+</style>
