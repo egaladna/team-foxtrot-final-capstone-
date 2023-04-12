074b3dc74c7243aa10cee450256c96956877d942
Idil Ahmed <idilahmed77@gmail.com>
2023-04-12 09:09:06 -0400

    push to update

---
+++ vue/src/components/TheFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <p>Digital Closet Footer</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+p {
+    text-align: center;
+    padding: 50px;
+}
+</style>
--- vue/src/components/TheHeader.vue
+++ vue/src/components/TheHeader.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-      <div class= "nav">
+      <nav id = "nav">
            <router-link v-bind:to="{ name: 'home' }">Home </router-link>
            <router-link v-if="$route.name != 'closet' " v-bind:to="{ name: 'closet' }">My Closet </router-link>
            <router-link v-if="$route.name == 'closet' || $route.name == 'outfits' " v-bind:to="{ name: 'selection' }">Choose Outfit </router-link>
            <router-link v-if="$route.name == 'closet' || $route.name == 'selection' " v-bind:to="{ name: 'outfits' }">My Outfits </router-link>
            <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-      </div>
+      </nav>
   </div>
 </template>
 
@@ -17,7 +17,17 @@ export default {
 </script>
 
 <style>
-.nav a{
+#nav {
+   display: flex;
+  justify-content: center;
+  padding: 20px;
+}
+#nav a{
     padding: 15px 24px;
+    color: black;
+}
+#nav a:hover {
+  background-color: black;
+  color: white;
 }
 </style>
