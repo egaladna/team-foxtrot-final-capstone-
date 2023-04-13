b20761d5d399f78fd5f99a72f4b4913baaed4c5a
Adna Egal <egaladna@gmail.com>
2023-04-13 14:08:45 -0400

    committing firebase closest attempt

--- vue/src/components/ClothesList.vue
+++ vue/src/components/ClothesList.vue
@@ -1,25 +1,33 @@
-<template> 
-<div id="closet">
-  <h1>My Closet</h1>
-  <div id = "clothing-items">
-    <div v-for="cloth in displayedClothes" v-bind:key="cloth.id">
-      <ClothingItem v-bind:cloth="cloth" />
-      <i class="fa fa-trash-o" title="Delete" style="font-style:24px;" v-on:click.prevent="deleteClothingItem(cloth.id)"></i>
-      <!-- <p id="msg"></p> -->
+<template>
+  <div id="closet">
+    <h1>My Closet</h1>
+    <div id="clothing-items">
+      <div v-for="cloth in displayedClothes" v-bind:key="cloth.id">
+        <ClothingItem v-bind:cloth="cloth" />
+        <input type="file" v-on:change="selectFile" />
+        <!-- <button v-on:click.prevent="uploadFile"></button> -->
+        <i
+          class="fa fa-trash-o"
+          title="Delete"
+          style="font-style: 24px"
+          v-on:click.prevent="deleteClothingItem(cloth.id)"
+        ></i>
+        <!-- <p id="msg"></p> -->
+      </div>
     </div>
   </div>
-
-</div>
 </template>
 
 <script>
 import ClothingItem from "@/components/ClothingItem";
 import ClosetService from "@/services/ClosetService";
+import {  storage } from "../main.js";
 
 export default {
   data() {
     return {
       clothing: [],
+      fileInput: {},
     };
   },
 
@@ -28,6 +36,35 @@ export default {
   },
 
   methods: {
+    // uploadFile(file) {
+    //   const storageRef = storage.ref();
+    //   const fileRef = storageRef.child(file.name);
+    //   return fileRef.put(file);
+    // },
+    // uploadFile() {
+    //   console.log(this.fileInput);
+    //const file = this.fileInput.files[0]
+    //   const storageRef = storage.ref()
+    //   console.log(storage.ref());
+    //   const fileRef = storageRef.child(this.fileInput.name)
+    //   fileRef.put(this.fileInput).then((response) => {
+    //     response.catch( err => {
+    //       console.error(err);
+    //     })
+    //     console.log('File uploaded successfully!')
+    //   }).catch( err => {
+    //     console.error(err);
+    //   })
+    // },
+    selectFile(event) {
+      console.log(event.target.files);
+      let temp = event.target.files[0];
+
+      const storageRef = storage().ref("fileInput/" + this.fileInput.name);
+
+      storageRef.put(temp);
+      console.log(this.fileInput);
+    },
     getClothesList() {
       ClosetService.getClothingList()
         .then((response) => {
@@ -39,19 +76,20 @@ export default {
     },
 
     deleteClothingItem(itemId) {
-      if (confirm("Are you sure you want to delete this garment? It will also be removed from any saved otufits.") == true) {
-        ClosetService.deleteClothingItem(itemId).then(response => {
-          if (response.status == 200) {
-            this.getClothesList();
-          }
-        }).catch(err => console.error(err));
-      } 
-			
+      if (
+        confirm(
+          "Are you sure you want to delete this garment? It will also be removed from any saved otufits."
+        ) == true
+      ) {
+        ClosetService.deleteClothingItem(itemId)
+          .then((response) => {
+            if (response.status == 200) {
+              this.getClothesList();
+            }
+          })
+          .catch((err) => console.error(err));
+      }
     },
-
-
-
-
   },
 
   computed: {
@@ -63,7 +101,7 @@ export default {
   },
 
   created() {
-      this.getClothesList();
+    this.getClothesList();
   },
 };
 </script>
--- vue/src/main.js
+++ vue/src/main.js
@@ -3,8 +3,28 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAze0hadhVuhuz9oCuM7GZhu7EpzP7hcGs",
+  authDomain: "te-foxtrot---digital-closet.firebaseapp.com",
+  projectId: "te-foxtrot---digital-closet",
+  storageBucket: "te-foxtrot---digital-closet.appspot.com",
+  messagingSenderId: "172605617246",
+  appId: "1:172605617246:web:ce281caded118c72afb0cf",
+  measurementId: "G-G1RPDK53JX"
+};
+
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app)
+
+
 
 Vue.config.productionTip = false
+
 
 axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
 
